9f2042cc14a44cfce7c75ff69b96cd4fc8bb38f0
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-27 20:52:15 +0100

    add swagger

--- src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
@@ -18,10 +18,8 @@ import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
-@EnableSwagger2
 open class ApplicationConfiguration {
 
     @Bean
@@ -58,4 +56,5 @@ open class ApplicationConfiguration {
                 .paths(PathSelectors.any())
                 .build()
     }
+
 }
