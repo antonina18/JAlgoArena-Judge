3b086b6910556d7c75fed4994724c267cd1eb9ba
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-27 19:05:25 +0100

    add swagger

--- src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
@@ -14,6 +14,10 @@ import com.jalgoarena.type.ListNode
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
@@ -45,4 +49,13 @@ open class ApplicationConfiguration {
             InMemoryJavaCompiler(),
             KotlinCompiler()
     )
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 }
